125f7702610f1a03f17e8fb59b3386f39b19a0f7
ahnzhx <modunaeggu@naver.com>
2020-06-19 15:55:50 +0900

    atomic test

--- src/basicConceptsOfThread/AtomicIntegerTest.java
+++ src/basicConceptsOfThread/AtomicIntegerTest.java
@@ -3,9 +3,18 @@ package basicConceptsOfThread;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerTest {
-    private AtomicInteger counter = new AtomicInteger();
+    /**
+     * TODO: ++, -- 연산 등에서만 사용할 수 있는 걸까?
+     */
+    private static AtomicInteger counter = new AtomicInteger();
 
-    public int getNextUniqueIndex(){
+    public static int getNextUniqueIndex(){
         return counter.getAndIncrement();
+    }
+
+    public static void main(String[] args) {
+        int returnValue = getNextUniqueIndex();
+        System.out.println(returnValue);
+
     }
 }

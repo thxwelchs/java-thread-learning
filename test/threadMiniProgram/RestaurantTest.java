dc104b0cdb7d37b416f79a4245e20b0745ca7cf1
ahnzhx <modunaeggu@naver.com>
2020-06-28 22:18:35 +0900

    reset module

--- test/threadMiniProgram/RestaurantTest.java
+++ test/threadMiniProgram/RestaurantTest.java
@@ -2,7 +2,6 @@ package threadMiniProgram;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
